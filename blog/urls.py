0d850c0c43ef6d0ff9f3b77ac1af9eded6da46df
Oli111 <olos.marek@gmail.com>
2025-02-17 19:26:25 +0100

    event_based_create

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ from .views import (
     BlogCreateView,
     EventListView,
     EventListDetailView,
+    EventListCreateView,
 )
 
 # from .views import BlogListView, BlogDetailView, LikeView, DisLikeView, BlogCreateView
@@ -26,9 +27,10 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
+    path('events/create/', EventListCreateView.as_view(), name='event_list_create'),
     path('events/', EventListView.as_view(), name='event_list'),
     path('events/<slug:slug>/', EventListDetailView.as_view(), name='event_list_detail'),
-    path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('<slug:event_slug>/create/', BlogCreateView.as_view(), name='blog_create'),
     path('<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
     path('like_post/<int:pk>', LikeView, name='like_post'),
     path('dislike_post/<int:pk>', DisLikeView, name='dislike_post'),
--- blog/views.py
+++ blog/views.py
@@ -33,12 +33,27 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import Post, EventList
 
+# class BlogCreateView(CreateView):
+#     model = Post
+#     fields = ['title', 'body', 'slug', 'author']  # Specify fields for the form
+#     template_name = 'blog/blog_form.html'
+#     success_url = reverse_lazy('blog:blog_list')  # Redirect after successful creation
+
 class BlogCreateView(CreateView):
     model = Post
-    fields = ['title', 'body', 'slug', 'author']  # Specify fields for the form
+    fields = ['title', 'body', 'slug', 'author']
     template_name = 'blog/blog_form.html'
-    success_url = reverse_lazy('blog:blog_list')  # Redirect after successful creation
 
+
+    def form_valid(self, form):
+        event_slug = self.kwargs.get('event_slug')  # Get the active EventList slug from URL
+        event_list = get_object_or_404(EventList, slug=event_slug)  # Retrieve the EventList
+        form.instance.event_list = event_list  # Automatically assign it
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.event_list.get_absolute_url()  # Redirect to event list page
+    
 class EventListView(ListView):
     model = EventList
     context_object_name = 'event_lists'
@@ -53,3 +68,9 @@ class EventListDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['posts'] = self.object.posts.all()  # Get posts related to this event list
         return context
+
+class EventListCreateView(CreateView):
+    model = EventList
+    fields = ['name', 'slug']  # Fields for the form
+    template_name = 'blog/event_list_form.html'
+    success_url = reverse_lazy('blog:event_list')  # Redirect back to event list after creation
